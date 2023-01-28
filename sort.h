9587f3d74c24b567aceb405a860b5fce378d160b
LowellUfot <lowellufot@gmail.com>
2023-01-28 09:24:00 -0800

    update to sort.h header file

--- sort.h
+++ sort.h
@@ -25,17 +25,12 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-<<<<<<< HEAD
-void shell_sort(int *array, size_t size);
-
-
-=======
 
 /*advance tasks*/
+void shell_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
->>>>>>> 12594916d788b454456c040cf2d718ef00cf4ba0
 
 #endif
