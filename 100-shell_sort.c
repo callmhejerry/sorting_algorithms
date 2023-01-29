724bab06411ca0afa7a2af18d95f4d1643dbb9da
LowellUfot <lowellufot@gmail.com>
2023-01-29 01:25:56 -0800

    update ti shell_sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -16,7 +16,7 @@ void shell_sort(int *array, size_t size)
 	/*initialise and define the gap interval, gap*/
 	size_t gap = 1;
 
-	if (array  == NULL || size == 0)
+	if (array  == NULL || size <= 1)
 		return;
 
 	while (gap <= size / 3)
